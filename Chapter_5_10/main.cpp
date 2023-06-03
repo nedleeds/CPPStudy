bccc8e83887c4634cb8909e6a2d554ce4b79fa18
dhl <lllee.dh@gmail.com>
2023-06-03 15:22:31 +0900

    [Fix] cin.ignore() + cin.get()

--- Chapter_5_10/main.cpp
+++ Chapter_5_10/main.cpp
@@ -1,19 +1,57 @@
 #include <iostream>
+#include <limits>
 using namespace std;
 
 int getInt();
 char getOperator();
 void printResult(int x, char op, int y);
+void cinGetTest();
 
 int main()
 {
-    int x = getInt();
-    int y = getInt();
-    int op = getOperator();
+    // int x = getInt();
+    // int y = getInt();
+    // int op = getOperator();
 
-    printResult(x, op, y);
+    // printResult(x, op, y);
+
+    cinGetTest();
 
     return 0;
+}
+
+void cinGetTest()
+{
+    // cin.get() 
+    // : 세 번째 매개변수로 지정한 종료문자 '\n' 을 만나면 읽기를 멈추지만, 
+    //   '\n' 을 입력 버퍼에서 제거하지는 않는다. 따라서, cin.get 이후
+    //   cin.ignore 을 통해 버퍼를 비워줄 수 있어야 한다
+    
+    // cin.ignore(streamsize n = 1, int delim = EOF)
+    // : Extracts characters from the input sequence and 
+    //   discards them, until either n characters have been 
+    //   extracted, or one compares equal to delim.
+
+
+    char test[50] = "";
+
+    cout << "Input: ";
+    cin.get(test, 50);
+    cout << test << endl;
+    // clear the input stream buffer till meet '\n'
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    cout << "Input: ";
+    cin.get(test, 50);
+    cout << test << endl;
+    // clear the input stream buffer till meet '\n'
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    
+    cout << "Input: ";
+    cin.get(test, 50);
+    cout << test << endl;
+    // clear the input stream buffer till meet '\n'
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
 int getInt()
